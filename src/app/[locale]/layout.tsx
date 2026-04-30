6b9f3cb57dce865fcb6771327e44e0b6904d9f95
roybrockington <r.brockington@gmail.com>
2026-04-30 12:09:15 +0100

    Favicon updated

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -25,7 +25,28 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Welcome to ZOOM - ZOOM EUROPE",
-  description: "Professional audio & video gear",
+  description: "ZOOM, ZOOM Corp, podcasting, sound design, music production, film production, mobile recorders, field recorders, podcast recorders, video recorders, iOS and Android recorders, audio interfaces, digital mixers, multi-effects devices, vocal processors, voice processors, vocal microphones, podcast microphones",
+    icons: {
+    icon: '//media.sound-service.eu/zoom/touch-icon-32.png',
+    shortcut: '//media.sound-service.eu/zoom/touch-icon-32.png',
+    apple: [
+      { url: '//media.sound-service.eu/zoom/touch-icon-57.png', sizes: '57x57' },
+      { url: '//media.sound-service.eu/zoom/touch-icon-72.png', sizes: '72x72' },
+      { url: '//media.sound-service.eu/zoom/touch-icon-114.png', sizes: '114x114' },
+      { url: '//media.sound-service.eu/zoom/touch-icon-144.png', sizes: '144x144' },
+    ],
+  },
+  alternates: {
+    canonical: 'https://www.zoom-europe.com/',
+    languages: {
+      'en': 'https://www.zoom-europe.com/',
+      'de': 'www.zoom-europe.com/de',
+      'fr': 'www.zoom-europe.com/fr',
+      'nl': 'www.zoom-europe.com/nl',
+      'pl': 'www.zoom-europe.com/pl',
+      'cs': 'www.zoom-europe.com/cz',
+    },
+  },
 };
 
 async function getCategories(locale: string): Promise<Category[]> {
